b43f7c8e7a3a9a50efe8c8dd36f27fde15b3dfb0
Nicolas Corbalan <outreef@hotmail.com>
2018-07-06 11:47:29 +0200

    setup second view and didmount

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Recipes = props => (
     <div className= "container">
         <div className= "row">
@@ -19,7 +21,11 @@ const Recipes = props => (
                                 {recipe.publisher}
                             </span></p>
                         </div>
-                        <button className="recipe_buttons" >View Recipe</button>
+                        <button className="recipe_buttons" >
+                            <Link to={{ pathname: `/recipe/${recipe.recipe_id}`,
+                            state: {recipe: recipe.title}
+                        }}>View Recipe</Link>
+                        </button>
                 </div>
             </div>
           );
